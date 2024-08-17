10156dfa74b89c9d6b9a60b7d8ea524864e99536
TronkIsHere <nguyenhuutrong11133@gmail.com>
2024-08-17 09:26:22 +0700

    Add new project with full setup

---
+++ smartContactManager/src/main/java/com/project/controllers/PageController.java
@@ -0,0 +1,13 @@
+package com.project.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+    @RequestMapping("/home")
+    public String home(){
+        System.out.println("Home page handler");
+        return "Home";
+    }
+}
